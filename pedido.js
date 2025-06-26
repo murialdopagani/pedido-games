04fc6b97d952d537b52ce1666c15177b481423d1
murialdopagani <murialdo.pagani@gmail.com>
2025-06-25 21:12:31 -0300

    Correção

--- pedido.js
+++ pedido.js
@@ -2,10 +2,11 @@ var total_quantidade = 0;
 var total_valor = 0;
 
 function adicionar() {
+  // Seleciona os elementos html
   const produto = $("#produto").val();
   const quantidade = $("#quantidade").val();
   const valor = $("#valor").val();
-  const codigo = $("#lista-produto tbody").length + 1;
+  const codigo = $("#lista-produto tbody tr").length + 1;
 
   total_quantidade += parseInt(quantidade);
   total_valor += parseFloat(valor) * parseInt(quantidade);
@@ -13,11 +14,22 @@ function adicionar() {
   $("#total_quantidade").text(total_quantidade);
   $("#total_valor").text(total_valor.toFixed(2));
 
+  // Criar uma linha da tabela de forma dinâmica
   const linha = `<tr>
     <td>${codigo}</td>
     <td>${produto}</td> 
-    <td>${valor}</td>
     <td>${quantidade}</td> 
+    <td>${valor}</td>
     </tr>`;
+
+  // Adicionar a linha no corpo da tabela
   $("#lista-produto tbody").append(linha);
+    
+  limpar();
+}
+
+function limpar() {
+  $("#produto").val("");
+  $("#quantidade").val("");
+  $("#valor").val("");
 }
